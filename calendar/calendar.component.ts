385cd2a90ec35e6325e275db581a6d87842c7238
Asaivani <50944451+Asaivani@users.noreply.github.com>
2019-07-19 09:07:20 +0530

    Add files via upload

---
+++ calendar/calendar.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit,ViewChild } from '@angular/core';
+import { FullCalendarComponent } from '@fullcalendar/angular';
+import { EventInput } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGrigPlugin from '@fullcalendar/timegrid';
+import interactionPlugin ,{ Draggable } from '@fullcalendar/interaction'; // for dateClick
+import { Calendar } from '@fullcalendar/core';
+
+
+@Component({
+  selector: 'app-calendar',
+  templateUrl: './calendar.component.html',
+  styleUrls: ['./calendar.component.scss']
+})
+export class CalendarComponent implements OnInit {
+events:any;
+  calendarComponent: FullCalendarComponent; // the #calendar in the template
+
+  calendarVisible = true;
+  calendarPlugins = [dayGridPlugin, timeGrigPlugin, interactionPlugin];
+  calendarWeekends = true;
+  calendarEvents: EventInput[] = [
+    { },
+  ];
+   
+ 
+
+  toggleVisible() {
+    this.calendarVisible = !this.calendarVisible;
+  }
+
+  toggleWeekends() {
+    this.calendarWeekends = !this.calendarWeekends;
+  }
+
+  gotoPast() {
+    let calendarApi = this.calendarComponent.getApi();
+    calendarApi.gotoDate('2000-01-01'); // call a method on the Calendar object
+  }
+
+     handleDateClick(arg) {
+    if (confirm('Would you like to add an event to ' + arg.dateStr + ' ?')) {
+      this.calendarEvents = this.calendarEvents.concat({ // add new event data. must create new array
+        title: prompt('Enter a Event'),
+        start: arg.date,
+        allDay: arg.allDay
+      })
+    }
+    
+    }
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+}
